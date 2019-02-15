b65af24e615a1cad5a3e54fa2149d01e97da1c94
App Academy Student <aa@Apps-Mac-mini-3.local>
2019-02-15 16:25:52 -0500

    Finish Phase 4: Linked-List

--- hashmap_skeleton/lib/p04_linked_list.rb
+++ hashmap_skeleton/lib/p04_linked_list.rb
@@ -1,4 +1,5 @@
 class Node
+  
   attr_reader :key
   attr_accessor :val, :next, :prev
 
@@ -20,7 +21,14 @@ class Node
 end
 
 class LinkedList
+  include Enumerable
+  attr_reader :head, :tail
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    self.head.next = self.tail
+    self.tail.prev = self.head
   end
 
   def [](i)
@@ -29,34 +37,74 @@ class LinkedList
   end
 
   def first
+    self.head.next
   end
 
   def last
+    self.tail.prev
   end
 
   def empty?
+    self.head.next == self.tail
   end
 
   def get(key)
+    current_node = self.head.next
+    until self.tail == current_node
+      return current_node.val if current_node.key == key 
+      current_node = current_node.next 
+    end
   end
 
   def include?(key)
+    current_node = self.head.next
+    until self.tail == current_node
+      return true if current_node.key == key 
+      current_node = current_node.next 
+    end
+    false
   end
 
   def append(key, val)
+    new_last_val = Node.new(key, val) #create E
+    last_val = self.tail.prev #get access to L from T's data
+    new_last_val.prev = last_val #link E to point to L
+    last_val.next = new_last_val #link L to point to E
+    new_last_val.next = self.tail #link E to point to T
+    self.tail.prev = new_last_val   #link T to point back to E
   end
 
   def update(key, val)
+    current_node = self.head.next
+    until self.tail == current_node
+      return current_node.val = val if current_node.key == key 
+      current_node = current_node.next 
+    end
   end
 
   def remove(key)
+    current_node = self.head.next
+    until self.tail == current_node
+      if current_node.key == key 
+        prev_node = current_node.prev
+        next_node = current_node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+      end 
+      current_node = current_node.next 
+    end
   end
 
   def each
+    current = self.head.next
+    until self.tail == current
+      yield current
+      current = current.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
