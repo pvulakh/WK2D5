ff42ee88388aa89f0936bb2c3fa629c14d250ec1
App Academy Student <aa@Apps-Mac-mini-3.local>
2019-02-15 15:09:20 -0500

    Finish Phase 2 & 3

--- hashmap_skeleton/lib/p02_hashing.rb
+++ hashmap_skeleton/lib/p02_hashing.rb
@@ -4,11 +4,27 @@ end
 
 class Array
   def hash
+     if self.empty?
+      0.hash
+     else 
+      self.inject do |acc, el|
+        acc *= el.hash # why does this work if multiplication is associative?
+      end
+    end 
   end
 end
 
 class String
   def hash
+    if self == ""
+      0.hash
+    else  
+      alphabet = ("a".."z").to_a
+      chars = self.chars.map {|char| alphabet.index(char.downcase) + 1 }
+      chars.inject do |acc, el|
+        acc *= el.hash
+      end
+    end 
   end
 end
 
@@ -17,5 +33,10 @@ class Hash
   # Make sure to implement an actual Hash#hash method
   def hash
     0
+    arr = self.sort_by {|k,v| k }
+    # arr.inject do |acc, el|
+    #   k, v = el  
+    #   acc += 2
+    # end
   end
 end
--- hashmap_skeleton/lib/p03_hash_set.rb
+++ hashmap_skeleton/lib/p03_hash_set.rb
@@ -1,24 +1,39 @@
 class HashSet
   attr_reader :count
+  attr_accessor :store
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
   end
 
-  def insert(key)
+  def insert(num)
+    unless self.include?(num.hash)
+      self[num] << num.hash
+      self.count += 1
+      resize! if self.count >= num_buckets
+    end
   end
 
-  def include?(key)
+  def remove(num)
+    if self.include?(num)
+      self[num].delete(num.hash) 
+      self.count -= 1
+    end
   end
 
-  def remove(key)
+  def include?(num)
+    self[num].include?(num.hash)
   end
+
+  protected
+  attr_writer :count
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    self.store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +41,13 @@ class HashSet
   end
 
   def resize!
+    all_nums = self.store.flatten
+    new_store = Array.new(num_buckets * 2) { Array.new }
+    self.store = new_store
+    self.count = 0
+
+    all_nums.each do |num|
+      self.insert(num)
+    end
   end
 end
